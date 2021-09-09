672b3d8d2c2eaa22456457651f88cb9666553595
Anuk Silva <vwv9667@autuni.ac.nz>
2021-09-10 01:47:25 +1200

    Re-implemented the Pause Menu and Added Pointer Hover and Sound FX for Hover and Button Click
    Re-implemented the Pause Menu and Added Pointer Hover and Sound FX for Hover and Button Click

--- De-Crypt It/Assets/Scripts/InGamePause/PauseGame.cs
+++ De-Crypt It/Assets/Scripts/InGamePause/PauseGame.cs
@@ -4,16 +4,17 @@ using UnityEngine;
 
 public class PauseGame : MonoBehaviour
 {
-    public Transform canvas;
+    public Transform pauseMenuCanvas;
+    public GameObject crosshairCanvas;
+    public GameObject Capsule;
+    public GameObject Flashlight;
+    public GameObject Camera;
     //public Transform Player;
     //public GameObject Environment;
     //public GameObject Player;
-    public GameObject Canvas;
-    public GameObject Capsule;
     //public GameObject Player;
-    public GameObject Flashlight;
-    public GameObject Camera;
     //public GameObject PauseMenuCamera;
+
     // Update is called once per frame
     void Update()
     {
@@ -27,35 +28,36 @@ public class PauseGame : MonoBehaviour
 
     public void Pause()
     {
-        if (canvas.gameObject.activeInHierarchy == false)
+        if (pauseMenuCanvas.gameObject.activeInHierarchy == false)
         {
             //Environment.SetActive(false);
             //Player.SetActive(false);
-            Canvas.SetActive(false);
-            canvas.gameObject.SetActive(true);
+            crosshairCanvas.SetActive(false);
+            pauseMenuCanvas.gameObject.SetActive(true);
             Capsule.SetActive(false);
             Flashlight.SetActive(false);
             //PauseMenuCamera.SetActive(true);
             Time.timeScale = 0; //pauses time, regular time is 1
             //Player.GetComponent<PlayerController>().enabled = false;
             //Player.GetComponent<CameraController>().enabled = false; //Getting script for player camera and disables
-            Camera.GetComponent<FlashlightController>().enabled = false; //Getting script for flashlight and disables
-
+            Camera.GetComponent<FirstPersonLook>().enabled = false; //Getting script for flashlight and disables
+            Camera.GetComponent<FlashlightController>().enabled = false;
         } 
         else
         {
             //Environment.SetActive(true);
             //Player.SetActive(true);
-            Canvas.SetActive(true);
-            canvas.gameObject.SetActive(false);
+            crosshairCanvas.SetActive(true);
+            pauseMenuCanvas.gameObject.SetActive(false);
             Time.timeScale = 1;
             Cursor.visible = false;
             Capsule.SetActive(true);
-            Flashlight.SetActive(true);
+            //Flashlight.SetActive(true);
             //PauseMenuCamera.SetActive(false);
             //Player.GetComponent<PlayerController>().enabled = true;
             //Player.GetComponent<CameraController>().enabled = true; //Getting script for player camera and enables
-            Camera.GetComponent<FlashlightController>().enabled = true; //Getting script for flashlight and enables
+            Camera.GetComponent<FirstPersonLook>().enabled = true; //Getting script for flashlight and enables
+            Camera.GetComponent<FlashlightController>().enabled = true;
         }
         
     }
