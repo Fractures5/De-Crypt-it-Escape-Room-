4ba3e18b5c624b2ad9ee3e05cf5a44623f12a496
Siddarath-Kumar <83688562+Siddarath-Kumar@users.noreply.github.com>
2021-09-09 18:49:33 +1200

    Modified Escape Menu and Script for Tutorial Mode
    - Modified the tutorial escape menu script
    - Added code to freeze player jump, camera movement and zoom scroll when player enters escape menu
    - Modified the tutorial mode environment with lighting and arena cover.

--- De-Crypt It/Assets/Scripts/MainMenu/TutorialEscapeMenu.cs
+++ De-Crypt It/Assets/Scripts/MainMenu/TutorialEscapeMenu.cs
@@ -7,25 +7,32 @@ public class TutorialEscapeMenu : MonoBehaviour
 {
     public bool isPaused = false;
     public GameObject pauseMenu;
-    //public GameObject firstPersonController;
-    //public GameObject firstPersonCamera;
+    public GameObject firstPersonController;
+    public GameObject firstPersonCamera;
 
     // Update is called once per frame
 
+    void Start()
+    {
+        Time.timeScale = 1;
+    }
 
     void Update()
     {
         // If "esc" is pressed
-        if(Input.GetButtonDown("Cancel"))
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
             //objectToDisable.SetActive(false);
             //Screen.lockCursor = true;
-            //Cursor.visible = true;
+            firstPersonController.GetComponent<Jump>().enabled=false;
+            firstPersonCamera.GetComponent<FirstPersonLook>().enabled=false;
+            firstPersonCamera.GetComponent<Zoom>().enabled=false;
 
             Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+
             
-            //firstPersonController.SetActive(false);
-            //firstPersonCamera.SetActive(true);
+            //firstPersonController.GetComponent("Jump").enabled=false;
             PauseTutorial();
             
         }
@@ -39,8 +46,8 @@ public class TutorialEscapeMenu : MonoBehaviour
             //Game is paused
             Time.timeScale = 0; 
             isPaused = true; // game is paused
-            Cursor.visible = true;
             pauseMenu.SetActive(true);
+            firstPersonController.GetComponent<Jump>().enabled=false;
         }
         else
         { 
@@ -49,25 +56,42 @@ public class TutorialEscapeMenu : MonoBehaviour
             Cursor.visible = false;
             isPaused = false;
             Time.timeScale = 1;
+            firstPersonController.GetComponent<Jump>().enabled=true;
+            firstPersonCamera.GetComponent<FirstPersonLook>().enabled=true;
+            firstPersonCamera.GetComponent<Zoom>().enabled=true;
         }
     }
 
     // Unpauses game once resume game is clicked
     public void returnToMainMenu()
     {
+        //pauseMenu.SetActive(false);
         Cursor.visible = true;
         //Cursor.lockState = CursorLockMode.Confined;
         //firstPersonController.SetActive(true);
-        SceneManager.LoadScene("MainMenu");
+
+        firstPersonController.GetComponent<Jump>().enabled=true;
+        firstPersonCamera.GetComponent<FirstPersonLook>().enabled=true;
+        firstPersonCamera.GetComponent<Zoom>().enabled=true;
+
+        SceneManager.LoadScene(0);
+       // Time.timeScale = 1;
         //return;
 
     }
 
     public void resumeTutorial()
     {
+        firstPersonController.GetComponent<Jump>().enabled=true;
+        firstPersonCamera.GetComponent<FirstPersonLook>().enabled=true;
+        firstPersonCamera.GetComponent<Zoom>().enabled=true;
+
         pauseMenu.SetActive(false);
         Cursor.visible = false;
         isPaused = false;
         Time.timeScale = 1;
+      
+        
+
     }
 }
