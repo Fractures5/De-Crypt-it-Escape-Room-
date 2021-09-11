98fe154cc3ab28d19abccbf9ef1e51dde132a2e1
Siddarath-Kumar <83688562+Siddarath-Kumar@users.noreply.github.com>
2021-09-12 00:22:41 +1200

    Implemented Player Interaction with TV to play Quiz (Errors still need to be fixed)
    - Created and Implemented the player interaction script so the quiz is loaded when the user presses "E" in close proximity to the TV.
    - Currently there is errors with the UI interaction Text and quiz task completion that needs to be to fixed.

--- De-Crypt It/Assets/Scripts/ObjectInteract/QuizLoad.cs
+++ De-Crypt It/Assets/Scripts/ObjectInteract/QuizLoad.cs
@@ -1,23 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class QuizLoad : MonoBehaviour
 {
     [SerializeField]
+    public Text interactionText;
+    public bool inRange = false;
+    public static bool taskComplete = false;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(taskComplete == false)
+        {
+            if(inRange == true && Input.GetKeyDown("e"))
+            {
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.None;
+                SceneManager.LoadScene(5);
+            }
+        }
     }
 
-    // void OnTriggerEnter(Collider other)
-    // {
-    //     if (other.CompareTag("Player"))
-    //     {
-    //         interactionText.gameObject.SetActive(true);
-    //         inRange = true;
-    //     }
-    // }
+    void OnTriggerEnter(Collider other)
+    {
+        
+        if(taskComplete == true)
+        {
+            inRange = false;
+            interactionText.gameObject.SetActive(false);
+        }
+        else
+        {
+            if (other.CompareTag("Player"))
+            {
+                inRange = true;
+                interactionText.gameObject.SetActive(true);
+            }
+        }
+    }
+
+    void OnTiggerExit(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            inRange = false;
+            interactionText.gameObject.SetActive(false);
+
+        }
+    }
 }
