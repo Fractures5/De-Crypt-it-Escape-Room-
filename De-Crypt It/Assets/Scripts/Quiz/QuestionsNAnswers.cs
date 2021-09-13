b88c68de85d19b0cdd21ad520f47207f5488ebd5
enricoserrano <69224805+enricoserrano@users.noreply.github.com>
2021-09-13 17:29:54 +1200

    Cleared folder to prevent merging issue

--- De-Crypt It/Assets/Scripts/Quiz/QuestionsNAnswers.cs
+++
@@ -1,9 +0,0 @@
-[System.Serializable] // To be able to use this in unity
-
-// Declares variables for holding questions, asnwers and the index
-public class QuestionsNAnswers
-{
-   public string Question;
-   public string[] Answers;
-   public int CorrectAnswer;
-}
