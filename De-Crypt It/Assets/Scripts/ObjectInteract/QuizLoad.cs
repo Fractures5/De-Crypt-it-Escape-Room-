e625b5793c4cf0f9179304ef619374d599d3fae8
Siddarath-Kumar <83688562+Siddarath-Kumar@users.noreply.github.com>
2021-09-15 20:55:42 +1200

    Added more questions to the quiz and refined the quiz script comments
    - Added more questions for the quiz and refined the other questions.
    - Added and refined the code comments on the quiz scripts.

--- De-Crypt It/Assets/Scripts/ObjectInteract/QuizLoad.cs
+++ De-Crypt It/Assets/Scripts/ObjectInteract/QuizLoad.cs
@@ -6,16 +6,29 @@ using UnityEngine.SceneManagement;
 
 public class QuizLoad : MonoBehaviour
 {
+    // Keeps track of the object color
     [SerializeField]
+    public Color startcolor;
+
+    // Text variable that gives instructions to the player on how to interact with the quiz
     public Text interactionText;
+
+    // Boolean which checks if the player is in the range of the tv object
     public bool inRange = false;
+
+    // Static variable which is readable and modifiable by other scripts. 
+    // It is used to determine if the quiz task is completed or not by the user.
     public static bool taskComplete = false;
+
+    // GameObject variables of the tv puzzle menu and clue which is used to set the gameobject to be active or not.
     public GameObject tvPuzzleMenu;
     public GameObject tvPuzzleClue;
-    public Color startcolor;
+   
     // Update is called once per frame
     void Update()
     {
+        // If the task is not complete and the user in range and interacts with the quiz 
+        // game object by pressing "E", then the quiz menu will load.
         if(taskComplete == false)
         {
             if(inRange == true && Input.GetKeyDown("e"))
@@ -27,14 +40,16 @@ public class QuizLoad : MonoBehaviour
         }
         else
         {
-            tvPuzzleMenu.SetActive(false);
-            tvPuzzleClue.SetActive(true);
+            // If the task is complete then....
+            tvPuzzleMenu.SetActive(false); // Disables the tv puzzle menu
+            tvPuzzleClue.SetActive(true); // Enables the tv clue
         }
     }
 
+    // This function is called when the user is close to the box collider of the gameobject in the TV
     void OnTriggerEnter(Collider other)
     {
-        
+        // If the task is complete, object is not higlighted, instructions is not shown and dont update the player within range
         if(taskComplete == true)
         {
             startcolor = GetComponent<Renderer>().material.color;
@@ -42,10 +57,12 @@ public class QuizLoad : MonoBehaviour
             inRange = false;
             interactionText.gameObject.SetActive(false);
         }
+        // If the task in not complete, object is highlighted, instructions text is show and the player within the range is updated
         else
         {
             if (other.CompareTag("Player"))
             {
+                // Highlights the interactable TV game object, changes the range status to true and activates the text instruction
                 startcolor = GetComponent<Renderer>().material.color;
                 GetComponent<Renderer>().material.color = Color.green;
                 inRange = true;
@@ -54,8 +71,11 @@ public class QuizLoad : MonoBehaviour
         }
     }
 
+    // This function is called when the user is exiting the box collider of the gameobject in the TV
     void OnTriggerExit(Collider other)
     {
+        // The color of the interactable gameobject in the TV is changed to the default color,
+        // the status of the range is changed to false, and the instructions text is disabled so the user cant see it.
         if(other.CompareTag("Player"))
         {
             GetComponent<Renderer>().material.color = startcolor;
