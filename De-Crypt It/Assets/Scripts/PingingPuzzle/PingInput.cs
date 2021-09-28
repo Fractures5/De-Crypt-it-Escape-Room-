cf77ed5b912715460b90d37914d7ef6b5dfdb9cb
Siddarath-Kumar <83688562+Siddarath-Kumar@users.noreply.github.com>
2021-09-28 17:50:24 +1300

    Implemented the successful ping message with animation delays
    - Created and implemented the messages for the successful ping along with the script to show the animation time delay of text being displayed to the user on the terminal like an actual terminal.

--- De-Crypt It/Assets/Scripts/PingingPuzzle/PingInput.cs
+++ De-Crypt It/Assets/Scripts/PingingPuzzle/PingInput.cs
@@ -7,9 +7,15 @@ public class PingInput : MonoBehaviour
 {
     // This script will check if the player has pinged correctly to unlock the keypad to unlock the door and escape.
     public string pingInput;
-    public string correctPing = "168.20.1";
+    public string correctPing = "ping 192.168.20.1";
     public GameObject inputField;
     public GameObject messageDisplay;
+    public GameObject pingPassedMsg1;
+    public GameObject pingPassedMsg2;
+    public GameObject pingPassedMsg3;
+    public GameObject pingPassedMsg4;
+    public GameObject pingPassedMsg5;
+    public GameObject successfulMsg;
 
     void Update()
     {
@@ -30,11 +36,28 @@ public class PingInput : MonoBehaviour
         {
             messageDisplay.GetComponent<Text>().text = "Ping successfu! - Door Keypad unlocked";
             // add code here to show the hidden text showing the congrats message and next steps.
+            StartCoroutine(messageCoroutine());
         }
         else
         {
             messageDisplay.GetComponent<Text>().text = "Ping unsuccessful - try again!";
             //add code here to show the hiddent text showing the unsuccesful ping message.
         }
+    }
+
+    IEnumerator messageCoroutine()
+    {
+        yield return new WaitForSeconds(2);
+        pingPassedMsg1.SetActive(true);
+        yield return new WaitForSeconds(1);
+        pingPassedMsg2.SetActive(true);
+        yield return new WaitForSeconds(1);
+        pingPassedMsg3.SetActive(true);
+        yield return new WaitForSeconds(1);
+        pingPassedMsg4.SetActive(true);
+        yield return new WaitForSeconds(1);
+        pingPassedMsg5.SetActive(true);
+        yield return new WaitForSeconds(2);
+        successfulMsg.SetActive(true);
     }
 }
