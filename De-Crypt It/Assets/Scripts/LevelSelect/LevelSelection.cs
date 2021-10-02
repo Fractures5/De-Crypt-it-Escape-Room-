ce8398b6a6c54355757716221a817158d481cc32
Siddarath-Kumar <83688562+Siddarath-Kumar@users.noreply.github.com>
2021-10-02 18:11:21 +1300

    Added button functionality to the level select UI with error handling
    - Modified the Level Select UI
    - Created and implemented the Level Select Scripts to the Level Select UI which work as intended with asking the user confirmations and it does error handling.

---
+++ De-Crypt It/Assets/Scripts/LevelSelect/LevelSelection.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class LevelSelection : MonoBehaviour
+{
+    public bool selectedEasyButton;
+    public bool selectedMediumButton;
+    public bool selectedHardButton;
+    public Button confirmButton;
+    string levelSelected;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        confirmButton.interactable = false;
+        selectedEasyButton = false;
+        selectedMediumButton = false;
+        selectedHardButton = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if (selectedEasyButton == true || selectedMediumButton == true || selectedHardButton == true)
+        // {
+        //     confirmButton.interactable = true;
+        // }
+        // else{
+        //     confirmButton.interactable = false;
+        // }
+    }
+
+    public void enableConfirmButton()
+    {
+        confirmButton.interactable = true;
+    }
+
+    public void easyClicked()
+    {
+        selectedEasyButton = true;
+        selectedMediumButton = false;
+        selectedHardButton = false;
+    }
+
+    public void mediumClicked()
+    {
+        selectedEasyButton = false;
+        selectedMediumButton = true;
+        selectedHardButton = false;
+    }
+
+    public void hardClicked()
+    {
+        selectedEasyButton = false;
+        selectedMediumButton = false;
+        selectedHardButton = true;
+    }
+
+    public void backgroundClicked()
+    {
+        Debug.Log("background clicked");
+        selectedEasyButton = false;
+        selectedMediumButton = false;
+        selectedHardButton = false;
+        confirmButton.interactable = false;
+    }
+
+    public void loadSelectedLevel()
+    {
+        Debug.Log("should switch scenes");
+        if(selectedEasyButton == true)
+        {
+            SceneManager.LoadScene("EasyGame");
+        }
+        else if(selectedMediumButton == true)
+        {
+            SceneManager.LoadScene("MediumGame");
+        }
+        else if(selectedHardButton == true)
+        {
+            SceneManager.LoadScene("MainGame"); // Load the hard level difficulty game
+        }
+    }
+    
+
+}
