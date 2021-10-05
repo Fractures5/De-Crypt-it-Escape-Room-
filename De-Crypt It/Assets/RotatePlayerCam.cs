5dfe56bb1a070789f8f506b8078dc94edffea84b
Anuk Silva <vwv9667@autuni.ac.nz>
2021-10-06 04:09:19 +1300

    Implemented Proper facing of the camera towards the box in the garage level
    When the player leaves the padlock scene, they will be facing the supply box of the garage level when loading back into the garage level.

--- De-Crypt It/Assets/RotatePlayerCam.cs
+++ De-Crypt It/Assets/RotatePlayerCam.cs
@@ -4,23 +4,27 @@ using UnityEngine;
 
 public class RotatePlayerCam : MonoBehaviour
 {
-    public GameObject box;
+    public Camera camera;
     void Start()
     {
-        //if (LockControl.isPadlockOpened == true)
-        //transform.LookAt(box.transform);
-        
+
     }
     void Update()
     {
+        if (LockControl.isPadlockOpened == true)
+        {
+            transform.Rotate(0, 180, 0);
+            camera.transform.Rotate(40, 0, 0);
+
+        }
         //if (LockControl.isPadlockOpened == true)
+        //{
+            //transform.rotation = Quaternion.Euler(0, 90, 0);
+            //transform.Rotate(0, 180, 0);
+           // camera.transform.Rotate(30, 0, 0);
+        //}
         //transform.LookAt(box.transform);
-
-
-        //transform.rotation = Quaternion.Euler(0, 90, 0);
-
-
-        transform.Rotate(0, 180, 0);
+        //camera.transform.Rotate(90, 180, 0);
         //transform.rotation = Quaternion.AngleAxis(30, Vector3.up);
         //Vector3 newRotation = new Vector3(90, 90, 90);
         //transform.eulerAngles = newRotation;
