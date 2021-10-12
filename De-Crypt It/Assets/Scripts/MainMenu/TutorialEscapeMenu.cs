4df7978da8848dd42e48f884361dc447bb2e5f93
Siddarath-Kumar <83688562+Siddarath-Kumar@users.noreply.github.com>
2021-10-13 01:49:04 +1300

    Implemented both flashlights to stop working in tutorial pause menu
    Implemented both flashlights to stop working in tutorial pause menu by editing the tutorial escape menu script.

--- De-Crypt It/Assets/Scripts/MainMenu/TutorialEscapeMenu.cs
+++ De-Crypt It/Assets/Scripts/MainMenu/TutorialEscapeMenu.cs
@@ -6,10 +6,19 @@ using UnityEngine.SceneManagement;
 public class TutorialEscapeMenu : MonoBehaviour
 {
     public bool isPaused = false;
+
     public GameObject pauseMenu;
     public GameObject firstPersonController;
     public GameObject firstPersonCamera;
+    public GameObject Capsule;
     public GameObject showMessage;
+    public GameObject Flashlight;
+    public GameObject UVLight;
+    public GameObject playerAudio;
+
+    public static bool isFlashlightOn = false;
+    public static bool isUVlightOn = false;
+
     //Unpauses the game when the script is first called
     void Start()
     {
@@ -18,13 +27,23 @@ public class TutorialEscapeMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // intialises the status of the UV and flash light
+        isFlashlightOn = FlashlightController.FlashlightActive;
+        isUVlightOn = TutorialUVLight.UvLightActive;
+
         // If "esc" is pressed, disables the player controls and invokes the pause tutorial function
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             firstPersonController.GetComponent<Jump>().enabled=false;
+            firstPersonController.GetComponent<FirstPersonMovement>().enabled=false;
             firstPersonCamera.GetComponent<FirstPersonLook>().enabled=false;
             firstPersonCamera.GetComponent<Zoom>().enabled=false;
+            firstPersonCamera.GetComponent<FlashlightController>().enabled = false;
+            firstPersonCamera.GetComponent<TutorialUVLight>().enabled = false;
             showMessage.GetComponent<TutorialInteraction>().enabled=false;
+            playerAudio.gameObject.SetActive(false);
+            Flashlight.SetActive(false);
+            Capsule.SetActive(false);
             Cursor.lockState = CursorLockMode.None;
             Cursor.visible = true;
             PauseTutorial();
@@ -49,11 +68,19 @@ public class TutorialEscapeMenu : MonoBehaviour
             pauseMenu.SetActive(false);
             Cursor.visible = false;
             isPaused = false;
+
             Time.timeScale = 1;
+            firstPersonController.GetComponent<FirstPersonMovement>().enabled=true;
             firstPersonController.GetComponent<Jump>().enabled=true;
             firstPersonCamera.GetComponent<FirstPersonLook>().enabled=true;
             firstPersonCamera.GetComponent<Zoom>().enabled=true;
+            firstPersonCamera.GetComponent<FlashlightController>().enabled = true;
+            firstPersonCamera.GetComponent<TutorialUVLight>().enabled = true;
             showMessage.GetComponent<TutorialInteraction>().enabled=true;
+            playerAudio.gameObject.SetActive(true);
+            Capsule.SetActive(true);
+            FlashlightController.FlashlightActive = isFlashlightOn;
+            TutorialUVLight.UvLightActive = isUVlightOn;
         }
     }
 
@@ -61,21 +88,33 @@ public class TutorialEscapeMenu : MonoBehaviour
     public void returnToMainMenu()
     {
         Cursor.visible = true;
+        firstPersonController.GetComponent<FirstPersonMovement>().enabled=true;
         firstPersonController.GetComponent<Jump>().enabled=true;
         firstPersonCamera.GetComponent<FirstPersonLook>().enabled=true;
         firstPersonCamera.GetComponent<Zoom>().enabled=true;
         showMessage.GetComponent<TutorialInteraction>().enabled=true;
+        firstPersonCamera.GetComponent<FlashlightController>().enabled = true;
+        firstPersonCamera.GetComponent<TutorialUVLight>().enabled = true;
+        playerAudio.gameObject.SetActive(true);
+        Capsule.SetActive(true);
         SceneManager.LoadScene(0);
         Time.timeScale = 1;
     }
     // Unpauses game once resume game is clicked and enables player controls
     public void resumeTutorial()
     {
+        firstPersonController.GetComponent<FirstPersonMovement>().enabled=true;
         firstPersonController.GetComponent<Jump>().enabled=true;
         firstPersonCamera.GetComponent<FirstPersonLook>().enabled=true;
         firstPersonCamera.GetComponent<Zoom>().enabled=true;
+        firstPersonCamera.GetComponent<FlashlightController>().enabled = true;
+        firstPersonCamera.GetComponent<TutorialUVLight>().enabled = true;
         showMessage.GetComponent<TutorialInteraction>().enabled=true;
+        playerAudio.gameObject.SetActive(true);
+        FlashlightController.FlashlightActive = isFlashlightOn;
+        TutorialUVLight.UvLightActive = isUVlightOn;
         pauseMenu.SetActive(false);
+        Capsule.SetActive(true);
         Cursor.visible = false;
         isPaused = false;
         Time.timeScale = 1;
