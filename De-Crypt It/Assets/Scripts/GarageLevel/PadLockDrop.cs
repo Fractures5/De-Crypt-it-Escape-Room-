a2a828e86e7e97814fe31f845a589e3e7053dd9b
LeoMac99 <leomac2014@gmail.com>
2021-10-12 18:36:33 +1300

    Updated with Anuk merge.

--- De-Crypt It/Assets/Scripts/GarageLevel/PadLockDrop.cs
+++ De-Crypt It/Assets/Scripts/GarageLevel/PadLockDrop.cs
@@ -7,25 +7,53 @@ public class PadLockDrop : MonoBehaviour
 {
     public Animation padlockFalling;
     public AnimationClip plDropping;
-    public bool hasClipPlayed;
+    public GameObject stuntDouble;
+    public static bool hasClipPlayed = false;
+    public static bool hasEnumeratorRan = false;
     // Start is called before the first frame update
     void Start()
     {
         padlockFalling = GetComponent<Animation>();
-        hasClipPlayed = false;
+        //hasClipPlayed = false;
 
-        if (LockControl.isPadlockOpened == true)
+        /**if (LockControl.isPadlockOpened == true && hasClipPlayed == false)
         {
             padlockFalling.clip = plDropping;
             padlockFalling.Play("PadlockFall");
             hasClipPlayed = true;
-        }
-        
+        }**/
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (LockControl.isPadlockOpened == true && hasClipPlayed == false)
+        {
+            padlockFalling.clip = plDropping;
+            padlockFalling.Play("PadlockFall");
+            hasClipPlayed = true;
+            StartCoroutine(WaitForFall(padlockFalling));
+        }
+        if (hasEnumeratorRan == true)
+        {
+            stuntDouble.gameObject.SetActive(true);
+            gameObject.SetActive(false);
+            //stuntDouble.gameObject.SetActive(true);
+            //gameObject.GetComponent<PadLockDrop>().enabled = false;
+        }
+
+        IEnumerator WaitForFall(Animation animation)
+        {
+            while (animation.isPlaying)
+            {
+                yield return null;
+            }
+            stuntDouble.gameObject.SetActive(true);
+            hasEnumeratorRan = true;
+        }
+    }
+
+    
         /**if (LockControl.isPadlockOpened == true && hasClipPlayed == false)
         {
             padlockFalling.clip = plDropping;
@@ -45,9 +73,9 @@ public class PadLockDrop : MonoBehaviour
         //}
         //if (LockControl.isPadlockOpened == true)
         //{
-            //padlockFalling.clip = plDropping;
-            //padlockFalling.Play("PadlockFall");
-       //}
-        
+        //padlockFalling.clip = plDropping;
+        //padlockFalling.Play("PadlockFall");
+        //}
+
+
     }
-}
