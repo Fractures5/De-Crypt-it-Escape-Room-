e425c4c03a7555dd8d3fe2dc5b17b008e916bdcb
Anuk Silva <vwv9667@autuni.ac.nz>
2021-10-13 20:54:58 +1300

    Added Battery Interaction Text popup in garage level
    If Player is in range of the battery, they will see a prompt on the UI saying that they can press "E" to pick up the battery.

--- De-Crypt It/Assets/Scripts/GarageLevel/BatteryInteraction.cs
+++ De-Crypt It/Assets/Scripts/GarageLevel/BatteryInteraction.cs
@@ -1,14 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BatteryInteraction : MonoBehaviour
 {
     //private Renderer renderer;
     public GameObject Battery;
     public static bool isBatteryCollected = false;
-    
-    private void OnMouseOver()
+    public Text batteryInteraction;
+    public bool isRange = false;
+
+    public bool battery;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && battery == true)
+        {
+            Battery.active = false;
+            isBatteryCollected = true;
+            batteryInteraction.gameObject.SetActive(false);
+        }
+    }
+
+    void OnTriggerEnter (Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (isRange == false)
+            {
+                batteryInteraction.gameObject.SetActive(false);
+            }
+            battery = true;
+            batteryInteraction.gameObject.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isRange = false;
+            battery = false;
+            batteryInteraction.gameObject.SetActive(false);
+        }
+
+    }
+    /*private void OnMouseOver()
     {
         
         if (Input.GetMouseButtonDown(0))
@@ -19,6 +57,6 @@ public class BatteryInteraction : MonoBehaviour
             //add text to allow the user to know
         }
         
-    }
+    }*/
 }
 
