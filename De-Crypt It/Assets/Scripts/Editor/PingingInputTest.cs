98796cc11b80287205600dacd571a159a6c7bc9e
Siddarath-Kumar <83688562+Siddarath-Kumar@users.noreply.github.com>
2021-10-15 21:32:16 +1300

    Updated password input script for Quiz to accept both cases V2
    Updated password input script for Quiz to accept both upper and lower cases.

---
+++ De-Crypt It/Assets/Scripts/Editor/PingingInputTest.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+
+public class PingingInputTest : MonoBehaviour
+{
+   [Test]
+   public void UserPingingInput_Test()
+   {
+       
+   }
+}
