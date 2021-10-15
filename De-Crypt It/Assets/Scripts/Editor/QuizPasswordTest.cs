79b948a4fcddfb49e1c6700294e01f3498511f31
Siddarath-Kumar <83688562+Siddarath-Kumar@users.noreply.github.com>
2021-10-15 22:11:53 +1300

    Completed and tested QuizPasswordInput unit testing
    Completed and tested QuizPasswordInput unit testing on the QuizPasswordTest.

---
+++ De-Crypt It/Assets/Scripts/Editor/QuizPasswordTest.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+
+public class QuizPasswordTest
+{
+   [Test]
+   public void QuizPasswordInput_PassUppercaseTest()
+   {
+
+      string passwordInput = "COMP602";
+      string expectedPasswordUpCase = "COMP602";
+      string expectedPasswordLowCase = "comp602";
+      bool loginSuccessful = false;
+
+      if (passwordInput == expectedPasswordUpCase || passwordInput == expectedPasswordLowCase)
+      {
+         loginSuccessful = true;
+         Debug.Log("Correct password was entered message will be shown to the user!");
+         Debug.Log("Start quiz button will need to be shown to the user.");
+         Debug.Log("Login boolean variable will be set to true.");
+
+      }
+      else
+      {
+         Debug.Log("Unsuccessful password attempt message will be shown to the user.");
+      }
+
+      Assert.AreEqual(expectedPasswordUpCase, passwordInput); 
+   }
+
+   [Test]
+   public void QuizPasswordInput_PassLowercaseTest()
+   {
+
+      string passwordInput = "comp602";
+      string expectedPasswordUpCase = "COMP602";
+      string expectedPasswordLowCase = "comp602";
+      bool loginSuccessful = false;
+
+      if (passwordInput == expectedPasswordUpCase || passwordInput == expectedPasswordLowCase)
+      {
+         loginSuccessful = true;
+         Debug.Log("Correct password was entered message will be shown to the user!");
+         Debug.Log("Start quiz button will need to be shown to the user.");
+         Debug.Log("Login boolean variable will be set to true.");
+
+      }
+      else
+      {
+         Debug.Log("Unsuccessful password attempt message will be shown to the user.");
+      }
+
+      Assert.AreEqual(expectedPasswordLowCase, passwordInput); 
+   }
+
+   [Test]
+   public void QuizPasswordInput_FailTest()
+   {
+
+      string passwordInput = "602comp";
+      string expectedPasswordUpCase = "COMP602";
+      string expectedPasswordLowCase = "comp602";
+      bool loginSuccessful = false;
+
+      if (passwordInput == expectedPasswordUpCase || passwordInput == expectedPasswordLowCase)
+      {
+         loginSuccessful = true;
+         Debug.Log("Correct password was entered message will be shown to the user!");
+         Debug.Log("Start quiz button will need to be shown to the user.");
+         Debug.Log("Login boolean variable will be set to true.");
+
+      }
+      else
+      {
+         Debug.Log("Unsuccessful password attempt message will be shown to the user.");
+      }
+
+      Assert.AreEqual(expectedPasswordUpCase, passwordInput); 
+   }
+   
+}
