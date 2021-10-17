589476f32edab6ec188027cab6c8729b938d6f23
Anuk Silva <vwv9667@autuni.ac.nz>
2021-10-17 18:59:45 +1300

    Added Comments to more scripts
    Added Comments to more scripts
    
    LockControl
    PadLockInitAnimation
    Rotate
    Target

--- De-Crypt It/Assets/Scripts/GarageLevel/LockControl.cs
+++ De-Crypt It/Assets/Scripts/GarageLevel/LockControl.cs
@@ -16,13 +16,14 @@ public class LockControl : MonoBehaviour
     }
     private void Start()
     {
+        //result array will store the clicked wheel number input from the player
         result = new int[]{0,0,0,0};
+        //defines correct combination with the set padlock code the player must input to unlock the padlock
         correctCombination = new int[] {5,1,9,3};
         Rotate.Rotated += CheckResults;
-        //turnTowardsBox = false;
-        //isPadlockOpened = false;
     }
 
+    //This function will check if the input from the player is correct and matches with the declared correct padlock combination
     private void CheckResults(string wheelName, int number)
     {
         switch (wheelName)
@@ -44,22 +45,24 @@ public class LockControl : MonoBehaviour
                 break;
         }
 
+        //checking if each result for each padlock number wheel matches with the correct combination
         if (result[0] == correctCombination[0] && result[1] == correctCombination[1]
             && result[2] == correctCombination[2] && result[3] == correctCombination[3] && !isPadlockOpened)
         {
             transform.position = new Vector3(transform.position.x, transform.position.y + 0.3f, transform.position.z);
+            //invoke the coroutine which will tell the player that they have successfully input the correct padlock combination
             StartCoroutine(correctCoroutine());
+            //set padlock opened bool to true
             isPadlockOpened = true;
             if (isPadlockOpened == true)
             {
                 turnTowardsBox = true;
             }
-            
-            Debug.Log("Padlock is unlocked now");
         }
 
     }
 
+    //IEnumerator function to display the correct pop up on in the padlock scene to let the player know they have input the correct padlock combination
     IEnumerator correctCoroutine()
     {
         correctImage.gameObject.SetActive(true);
