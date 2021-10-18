b89a81516dd88709abe2451547345f4694aefa98
Anuk Silva <vwv9667@autuni.ac.nz>
2021-10-18 15:47:53 +1300

    Reimplemented Sid/Enrico Fixes
    Reimplemented Sid/Enrico Fixes
    
    Tutorial Instruction Card Timer stuff
    Test cases for enrico's leaderboard
    Test cases for sid's quiz password input

--- De-Crypt It/Assets/Scripts/ObjectInteract/WinScreenTrigger.cs
+++ De-Crypt It/Assets/Scripts/ObjectInteract/WinScreenTrigger.cs
@@ -21,39 +21,48 @@ public class WinScreenTrigger : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            if(saveEasy == true)
+            if (saveEasy == true)
             {
-                easyTimer();
+                calculateTimeLeft(TimerCountdown.timeLeft);
+                //Saves the player result into the easy difficulty leaderboard script
+                HighScoresEasy.trackuserName = playernameInput;
+                HighScoresEasy.trackuserScore = int.Parse(overallTimeLeft);
                 //saveEasy = false;
             }
             else if(saveHard == true)
             {
-                hardtimer();
+                calculateTimeLeft(TimerCountdown.timeLeft);
+                //Saves the player result into the hard difficulty leaderboard script
+                HighScores.trackuserName = playernameInput;
+                HighScores.trackuserScore = int.Parse(overallTimeLeft);
                 //saveHard = false;
             }
             else if(saveMedium == true)
             {
-                mediumTimer();
+                calculateTimeLeft(TimerCountdown.timeLeft);
+                HighScoresMedium.trackuserName = playernameInput;
+                HighScoresMedium.trackuserScore = int.Parse(overallTimeLeft);
                 //saveMedium = false;
             }
             SceneManager.LoadScene("WinScreen");
         }
     }
-    //this is for hard mode
-    public void hardtimer()
+
+    public void calculateTimeLeft(int timeRemaining)
     {
         //Acesses the time left in the time script
-        timeLeft = TimerCountdown.timeLeft;
+        timeLeft = timeRemaining;
+        //timeLeft = TimerCountdown.timeLeft;
         //Performs calculation to get the minutes left and seconds left using the time left in the time script
         minutesLeft = Mathf.FloorToInt(timeLeft / 60F);
         secondsLeft = Mathf.FloorToInt(timeLeft - minutesLeft * 60);
-
+ 
         //Accesses the winning screen to show the player result
         ShowEndResult.recordMinutesLeft = minutesLeft.ToString();
         ShowEndResult.recordSecondsLeft = secondsLeft.ToString();
 
         overallTimeLeft = minutesLeft.ToString() + secondsLeft.ToString();
-
+ 
         //Checks if the result has 0 infront, since integer takes the 0 at the front, we are using .tostring to add 0
         //This is needed as we want to format inconsistent integer produced by the data
         if(secondsLeft < 10 && minutesLeft < 10)
@@ -72,84 +81,5 @@ public class WinScreenTrigger : MonoBehaviour
             overallTimeLeft = "0" + minutesLeft.ToString() + secondsLeft.ToString();
             ShowEndResult.recordMinutesLeft = "0" + minutesLeft.ToString();
         }
-        //Saves the player result into the hard difficulty leaderboard script
-        HighScores.trackuserName = playernameInput;
-        HighScores.trackuserScore = int.Parse(overallTimeLeft);
-    }
-
-    public void mediumTimer()
-    {
-        //Acesses the time left in the time script
-        timeLeft = TimerCountdown.timeLeft;
-
-        //Performs calculation to get the minutes left and seconds left using the time left in the time script
-        minutesLeft = Mathf.FloorToInt(timeLeft / 60F);
-        secondsLeft = Mathf.FloorToInt(timeLeft - minutesLeft * 60);
-
-        //Accesses the winning screen to show the player result
-        ShowEndResult.recordMinutesLeft = minutesLeft.ToString();
-        ShowEndResult.recordSecondsLeft = secondsLeft.ToString();
-
-        overallTimeLeft = minutesLeft.ToString() + secondsLeft.ToString();
-
-        //Checks if the result has 0 infront, since integer takes the 0 at the front, we are using .tostring to add 0
-        //This is needed as we want to format inconsistent integer produced by the data
-        if(secondsLeft < 10 && minutesLeft < 10)
-        {
-            overallTimeLeft = "0" + minutesLeft.ToString() + "0" + secondsLeft.ToString();
-            ShowEndResult.recordMinutesLeft = "0" + minutesLeft.ToString();
-            ShowEndResult.recordSecondsLeft = "0" + secondsLeft.ToString();
-        }
-        else if(secondsLeft < 10)
-        {
-            overallTimeLeft = minutesLeft.ToString() + "0" + secondsLeft.ToString();
-            ShowEndResult.recordSecondsLeft = "0" + secondsLeft.ToString();
-        }
-        else if(minutesLeft < 10)
-        {
-            overallTimeLeft = "0" + minutesLeft.ToString() + secondsLeft.ToString();
-            ShowEndResult.recordMinutesLeft = "0" + minutesLeft.ToString();
-        }
-        //Saves the player result into the medium difficulty leaderboard script
-        HighScoresMedium.trackuserName = playernameInput;
-        HighScoresMedium.trackuserScore = int.Parse(overallTimeLeft);
-    }
-
-    public void easyTimer()
-    {
-        //Acesses the time left in the time script
-        timeLeft = TimerCountdown.timeLeft;
-        
-        //Performs calculation to get the minutes left and seconds left using the time left in the time script
-        minutesLeft = Mathf.FloorToInt(timeLeft / 60F);
-        secondsLeft = Mathf.FloorToInt(timeLeft - minutesLeft * 60);
-
-        //Accesses the winning screen to show the player result
-        ShowEndResult.recordMinutesLeft = minutesLeft.ToString();
-        ShowEndResult.recordSecondsLeft = secondsLeft.ToString();
-
-        overallTimeLeft = minutesLeft.ToString() + secondsLeft.ToString();
-
-        //Checks if the result has 0 infront, since integer takes the 0 at the front, we are using .tostring to add 0
-        //This is needed as we want to format inconsistent integer produced by the data
-        if(secondsLeft < 10 && minutesLeft < 10)
-        {
-            overallTimeLeft = "0" + minutesLeft.ToString() + "0" + secondsLeft.ToString();
-            ShowEndResult.recordMinutesLeft = "0" + minutesLeft.ToString();
-            ShowEndResult.recordSecondsLeft = "0" + secondsLeft.ToString();
-        }
-        else if(secondsLeft < 10)
-        {
-            overallTimeLeft = minutesLeft.ToString() + "0" + secondsLeft.ToString();
-            ShowEndResult.recordSecondsLeft = "0" + secondsLeft.ToString();
-        }
-        else if(minutesLeft < 10)
-        {
-            overallTimeLeft = "0" + minutesLeft.ToString() + secondsLeft.ToString();
-            ShowEndResult.recordMinutesLeft = "0" + minutesLeft.ToString();
-        }
-        //Saves the player result into the easy difficulty leaderboard script
-        HighScoresEasy.trackuserName = playernameInput;
-        HighScoresEasy.trackuserScore = int.Parse(overallTimeLeft);
     }
 }
